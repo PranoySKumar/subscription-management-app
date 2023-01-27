0e78e3bb6715ad5d8fa5c0a2e8936f501136407c
PranoySKumar <pranoyskumar@gmail.com>
2023-01-27 14:07:44 +0530

    added product asset images

---
+++ lib/utils/asset_repo.dart
@@ -0,0 +1,5 @@
+class AssetRepo {
+  static const String product1 = "assets/images/product_1.png";
+  static const String product2 = "assets/images/product_2.png";
+  static const String product3 = "assets/images/product_3.png";
+}
