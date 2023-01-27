5bc3b893e21841d30a6775fc63cdbed5831f7ff5
PranoySKumar <pranoyskumar@gmail.com>
2023-01-27 22:02:00 +0530

    added filter and search icons

--- lib/utils/asset_repo.dart
+++ lib/utils/asset_repo.dart
@@ -2,4 +2,8 @@ class AssetRepo {
   static const String product1 = "assets/images/product_1.png";
   static const String product2 = "assets/images/product_2.png";
   static const String product3 = "assets/images/product_3.png";
+
+  //icons
+  static const String filterIcon = "assets/icons/filter_icon.svg";
+  static const String searchIcon = "assets/icons/search_icon.svg";
 }
