cf0d849f40504286f05ef8528eedadfb6fd9a3b8
PranoySKumar <pranoyskumar@gmail.com>
2023-01-28 09:40:40 +0530

    added title large to theme data

--- lib/theme/light_theme.dart
+++ lib/theme/light_theme.dart
@@ -12,5 +12,6 @@ final lightThemeData = ThemeData(
       bodyLarge: TextStyle(fontSize: 16, color: _darkGreenColor),
       bodyMedium: TextStyle(fontSize: 14, color: _darkGreenColor),
       bodySmall: TextStyle(fontSize: 12, color: _darkGreenColor),
+      titleLarge: TextStyle(fontSize: 20, color: _darkGreenColor, fontWeight: FontWeight.w600),
       titleMedium: TextStyle(fontSize: 16, color: _darkGreenColor, fontWeight: FontWeight.w600)),
 );
